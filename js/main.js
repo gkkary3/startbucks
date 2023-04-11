5a795e0b1b02d5bec7465069d6e2f36bb48b5f2d
DESKTOP-332GF4V\kkary <nambawon1@naver.com>
2023-04-12 00:10:59 +0900

    Show elements sequentially

--- js/main.js
+++ js/main.js
@@ -34,3 +34,14 @@ window.addEventListener('scroll', _.throttle(function() {
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+  // 애니메이션 메서드
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1)*.7, //0.7 1.4 2.1. 2.7초 뒤에 순차적으로 delay
+    opacity: 1
+  });
+});
